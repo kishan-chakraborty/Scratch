2a72c04f4d995dd52b53840122daf4ddd5a051bb
kishan-chakraborty <kishanchakraborty833@gmail.com>
2024-04-27 09:51:32 +0530

    linear regression explanation and code [incomplete]

--- ClassicalML/linear_regression.py
+++ ClassicalML/linear_regression.py
@@ -19,7 +19,7 @@ class LinearRegression:
     def __init__(self,
                  lr: float=0.01,
                  n_iters: int=1000,
-                 solver: str = 'OLS',
+                 solver: str = 'gradient_descent',
                  reg: str = None):
         self.lr = lr            # learning rate for gradient descent.
         self.n_iters = n_iters  # number of iterations.
@@ -27,17 +27,20 @@ class LinearRegression:
         self.reg = reg          # Regularization technique if any.
         self.x = None           # training predictor variables.
         self.y = None           # training response variables.
+        self.n_sample = None    # No. of samples in the training data.
+        self.n_features = None  # No. of predictor features.
         self.weights = None     # Calculated regression weight [slope]
         self.bias = None        # Calculated bias [intercept]
 
-    def fit(self, X, y):
+    def fit(self, x, y):
         """
         Args:
-            X: training predictor variables [n_sample, n_features]
+            x: training predictor variables [n_sample, n_features]
             y: training response variables [n_sample]
         """
-        self.X = X
+        self.x = x
         self.y = y
+        self.n_features, self.n_sample = self.x.shape
         if self.solver == 'ols':
             self.ols_solver()
         else:
@@ -52,13 +55,60 @@ class LinearRegression:
         weights (beta_hat) = (X^TX)^-1 X^Ty
         bias (beta_0) = avg(y-X*beta_hat)
         """
-        self.weights = np.linalg.inv(self.x.T@self.x) @ (self.x.T @ self.y)
-        self.bias = np.avg(self.y-(self.x@self.weights))
+        if self.reg is None:
+            self.weights = np.linalg.inv(self.x.T@self.x) @ (self.x.T @ self.y)
+            self.bias = np.mean(self.y-(self.x@self.weights))
+        else:
+            if self.reg == 'ridge':
+                self.ridge_solver()
+            else:
+                print('''Lasso do not have a closed form solution.\n
+                      Solving using gradient descent instead.''')
+                self.lasso_solver()
+
+    def ridge_solver(self):
+        """
+                    y = X*beta + beta0 + lambda*(beta^Tbeta)
+        Solving the linear regression problem with regularization.
+        Regularization becomes necessary to overcome model overfitting.
+        Even for a simple model like linear regression over fitting can be a problem
+        especially when no. of features is ver high compared to availavle training smaples. 
+
+        The closed form solution is given by:
+            beta(λ)=(X^T X + λI)^-1 (X^T y)
+            beta0 = avg(y-X*beta)
+        """
+        if self.solver == 'OLS':
+            reg_param = 0.01
+            xtx = self.x.T@self.x
+            self.weights = np.linalg.inv(xtx + reg_param*np.ones_like(xtx)) @ (self.x.T@self.y)
+            self.bias = np.mean(self.y-(self.x@self.weights))
+        else:
+            self.weights= np.random.rand(self.n_features)
+            self.bias= 0
+
+            for _ in range(self.n_iters):
+                y_predicted= np.dot(self.x, self.weights)
+                self.weights-= (self.lr/ self.n_sample)*(self.x.T).dot(y_predicted- self.y)
+                self.bias-= (self.lr/ self.n_sample)*np.sum(y_predicted- self.y)
+
+
+    def lasso_solver(self):
+        pass
 
     def gradient_descent_solver(self):
-        loss = sum((self.y - self.x @ self.weights - self.bias) ** 2)
+        """
+        Solve the linear regression problem using gradient descent algorithm.
+        This is a implementation of batch gradient descent.
+        TO DO: Implement stpchastic or mini batch gradient algorithm
+        """
+        self.weights= np.random.rand(self.n_features)
+        self.bias= 0
+
         for _ in range(self.n_iters):
-            pass
+            y_predicted= np.dot(self.x, self.weights)
+            self.weights-= (self.lr/ self.n_sample)*(self.x.T).dot(y_predicted- self.y)
+            self.bias-= (self.lr/ self.n_sample)*np.sum(y_predicted- self.y)
 
     def predict(self, X):
         pass
